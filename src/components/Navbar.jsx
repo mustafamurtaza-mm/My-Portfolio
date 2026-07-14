2334729b518e28a3b9d332d50db06520c0517898
Mustafa Murtaza <mustafamurtaza032@gmail.com>
2026-07-14 12:43:35 +0500

    Updated Portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,48 @@
+import { useState } from "react";
+
 function Navbar(){
+
+const [open,setOpen] = useState(false);
 
 return(
 
 <nav className="fixed top-0 w-full bg-gray-950/80 backdrop-blur z-50">
 
-<div className="max-w-6xl mx-auto flex justify-between p-5">
+<div className="max-w-6xl mx-auto flex justify-between items-center p-5">
 
 <h1 className="text-2xl font-bold">
 Mustafa.dev
 </h1>
 
 
-<div className="space-x-6">
+<button
+className="md:hidden text-2xl"
+onClick={()=>setOpen(!open)}
+>
+☰
+</button>
+
+
+<div
+className={`${
+open ? "block" : "hidden"
+} md:flex space-x-6 absolute md:static top-16 left-0 w-full md:w-auto bg-gray-950 md:bg-transparent p-5 md:p-0`}
+>
+
 
 <a href="#about">About</a>
 
 <a href="#skills">Skills</a>
 
-<a href="#projects">Projects</a>
-
 <a href="#services">Services</a>
+
+<a href="#projects">Projects</a>
 
 <a href="#contact">Contact</a>
 
+
 </div>
+
 
 </div>
 
